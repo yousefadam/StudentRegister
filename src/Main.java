98f863948c274aba5c4a6acc29813f7527c9dd57
Yousef <yousef.adam2001@gmail.com>
2023-05-19 02:44:29 +0300

    added the Batch add functionality

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -18,7 +22,7 @@ public class Main {
                     addStudentData(scanner);
                     break;
                 case 2:
-                    selectBatchFile();
+                    addBatchStudentsData(scanner);
                     break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
@@ -45,14 +49,57 @@ public class Main {
         System.out.print("Enter student courses (comma-separated): ");
         String courses = scanner.nextLine();
 
-        // Process the entered data
-        System.out.println("Student Name: " + name);
-        System.out.println("Student ID: " + id);
-        System.out.println("Student Courses: " + courses);
+        // Format the student data
+        String studentData = name + ";" + id + ";" + courses;
+
+        // Write student data to a CSV file
+        try {
+            FileWriter fileWriter = new FileWriter("/app/data/batch/Data.csv", true);
+            fileWriter.write(studentData + "\n");
+            fileWriter.close();
+            System.out.println("Student data added to file: students.csv");
+        } catch (IOException e) {
+            System.out.println("Error occurred while writing to the file: " + e.getMessage());
+        }
     }
 
-    private static void selectBatchFile() {
-        String folderPath = "/app/data/batch";
+    private static void addBatchStudentsData(Scanner scanner) {
+        String folderPath = "E:\\yousef\\College\\Year 4\\Semester 2\\Cloud\\Assignment 2\\Student files";
+
+        // Get a list of eligible batch files
+        List<String> eligibleFiles = getEligibleBatchFiles(folderPath);
+
+        // Display eligible batch files to the user
+        System.out.println("Eligible Batch Files:");
+        for (int i = 0; i < eligibleFiles.size(); i++) {
+            System.out.println(eligibleFiles.get(i));
+        }
+
+        // Prompt the user to select a file
+        System.out.print("Enter the Name of the batch file to use: ");
+        String fileName = scanner.nextLine();
+
+        // Get the selected file path
+        String selectedFilePath = folderPath + fileName;
+
+        // Read the content of the selected file
+        try {
+            List<String> fileContent = Files.readAllLines(Paths.get(selectedFilePath));
+
+            // Write each line from the selected file to the target file
+            FileWriter fileWriter = new FileWriter("/app/data/batch/Data.csv", true);
+            for (String line : fileContent) {
+                fileWriter.write(line + "\n");
+            }
+            fileWriter.close();
+
+            System.out.println("Student data added to file: Data.csv");
+        } catch (IOException e) {
+            System.out.println("Error occurred while reading or writing the files: " + e.getMessage());
+        }
+    }
+
+    private static List<String> getEligibleBatchFiles(String folderPath) {
         List<String> eligibleFiles = new ArrayList<>();
 
         File folder = new File(folderPath);
@@ -65,5 +112,7 @@ public class Main {
                 }
             }
         }
+
+        return eligibleFiles;
     }
 }
